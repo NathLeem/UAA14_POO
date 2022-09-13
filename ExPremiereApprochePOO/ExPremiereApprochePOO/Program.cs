294099226f87a97cddd89efaf40839a35dff4013
Leemans Nathan <200253@CCM-Asty-Moulin.local>
2022-09-13 15:21:20 +0200

    fa

---
+++ ExPremiereApprochePOO/ExPremiereApprochePOO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExPremiereApprochePOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            chien[] mesChiens = new chien[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Quel est le nom de votre chien ?");
+                string nomChien = Console.ReadLine();
+                Console.WriteLine("Quel est l'âge de votre chien ?");
+                uint ageChien = uint.Parse(Console.ReadLine());
+                Console.WriteLine("Quel est la race de votre chien ?");
+                string raceChien = Console.ReadLine();
+
+                mesChiens[i] = new chien(nomChien, ageChien, raceChien);
+                Console.WriteLine(mesChiens[i].AfficheCaracteristiques());
+            }
+        }
+    }
+}
---
+++ ExPremiereApprochePOO/ExPremiereApprochePOO/chien.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExPremiereApprochePOO
+{
+    class chien
+    {
+        private string _nom;
+        private uint _age;
+        private string _race;
+
+        public chien(string _nom, uint _age, string _race)
+        { 
+            this._nom = _nom;
+            this._age = _age;
+            this._race = _race;
+        }
+        public string AfficheCaracteristiques()
+        {
+            string phrase = "Nom: " + this._nom + " Age: " + this._age + " Race: " + this._race;
+            return phrase;
+        }
+    }
+}
