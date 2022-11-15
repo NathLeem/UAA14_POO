1aa2190730cbfe85e5fc7b1b3306c9fe741bbf6c
Leemans Nathan <200253@CCM-Asty-Moulin.local>
2022-11-15 14:16:56 +0100

    cercle

---
+++ act4_cercle/act4_cercle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace act4_cercle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
